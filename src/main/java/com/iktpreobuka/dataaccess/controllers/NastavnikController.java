df20fa2815284292d5e8c6bba0dce286c22f7c59
Ivanica91 <ivana.ristic.brains22@gmail.com>
2023-06-17 09:42:31 +0200

    dodatCrossOriginUNastavniku

--- src/main/java/com/iktpreobuka/dataaccess/controllers/NastavnikController.java
+++ src/main/java/com/iktpreobuka/dataaccess/controllers/NastavnikController.java
@@ -8,6 +8,7 @@ package com.iktpreobuka.dataaccess.controllers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +22,7 @@ import com.iktpreobuka.dataaccess.services.NastavnikService;
 
 @RestController
 @RequestMapping("/nastavnik")
+@CrossOrigin(origins = "http://localhost:3000")
 public class NastavnikController {
 	
 	@Autowired
